7cb4b969ecf6380c30cef9c99db975eb6d1115ab
Andres Suarez <suarezandres296@gmail.com>
2025-10-15 11:32:55 -0300

    a

--- os_example.js
+++ os_example.js
@@ -3,6 +3,7 @@ const os = require("os");
 const plataforma = os.platform();
 console.log("Plataforma del sistema operativo:", plataforma);
 
+
 const memoriaLibre = os.freemem();
 console.log("Memoria libre disponible (bytes):", memoriaLibre);
 
@@ -10,6 +11,4 @@ const memoriaTotal = os.totalmem();
 console.log("Memoria total del sistema (bytes):", memoriaTotal);
 
 const cpuinfo = os.cpus();
-console.log("Número de núcleos de CPU:", cpuinfo.length);
-console.log("Modelo de CPU:", cpuinfo[0].model);
-console.log("Velocidad de CPU (MHz):", cpuinfo[0].speed);
+console.log("Información de las CPU:", cpuinfo);
